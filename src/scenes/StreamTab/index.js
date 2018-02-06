ec5e52bfe99226cbc4c2d6753ef57d60cb3b2d81
artemkha <khaapt@gmail.com>
2018-02-06 22:09:24 +0300

    tab > open-close

--- src/scenes/StreamTab/index.js
+++ src/scenes/StreamTab/index.js
@@ -1,18 +1,48 @@
-import React from 'react';
-// import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import './StreamTab.css';
+  // import {
+  //   Glyphicon,
+  // } from 'react-bootstrap';
 
-export default () => (
+export default class StreamTab extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isOpen: false,
+    }
+  }
 
-  <div className="tab_container">
-    <div className="stream_tab">
-      <h4 className="stream_tab__header">Schema</h4>
-      <div className="stream_tab__container">
-        <div className="stream_tab__instance"></div>
-        <div className="stream_tab__instance"></div>
-        <div className="stream_tab__instance"></div>
-        <div className="stream_tab__instance"></div>
+  open = () => {
+    this.setState({ isOpen: true });
+  }
+
+  close = () => {
+    this.setState({ isOpen: false });
+  }
+
+  render() {
+    const { isOpen } = this.state;
+    const TabToggle = () => (
+      <div className="stream_tab__toggle" onClick={this.open}>
+        Stream
       </div>
-    </div>
-  </div>
-);
+    );
+
+    if (!isOpen) return <TabToggle />;
+    
+    return (
+      <div className="tab_container">
+        <div className="stream_tab">
+          <span onClick={this.close} className="stream_tab__close"></span>
+          <h4 className="stream_tab__header">Schema</h4>
+          <div className="stream_tab__container">
+            <div className="stream_tab__instance"></div>
+            <div className="stream_tab__instance"></div>
+            <div className="stream_tab__instance"></div>
+            <div className="stream_tab__instance"></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
