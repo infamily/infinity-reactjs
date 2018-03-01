5ed92567c507eae23d58e15d299fc7e1521714ae
artemkha <khaapt@gmail.com>
2018-03-01 22:39:28 +0300

    program toggle > to top

--- src/components/ProgramToggle/index.jsx
+++ src/components/ProgramToggle/index.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import TooltipOverlay from 'components/TooltipOverlay';
 import './ProgramToggle.css';
 
-const ProgramToggle = () => (
-  <div className="program_toggle">
-    <TooltipOverlay text="importing programs will be released coming soon..." placement="bottom">
-      <span>Programs</span>
-    </TooltipOverlay>
-  </div>
-);
+const ProgramToggle = () => {
+  const toTop = () => window.scrollTo(0, 0);
+  const text = "(click to go to top) Importing programs will be released coming soon...";
+  
+  return (
+    <div className="program_toggle" onClick={toTop}>
+      <TooltipOverlay text={text} placement="bottom">
+        <span>Programs</span>
+      </TooltipOverlay>
+    </div>
+  );
+}
 
 export default ProgramToggle;
