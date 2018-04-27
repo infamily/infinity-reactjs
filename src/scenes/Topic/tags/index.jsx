98ac7bfda8130486cd7deef8b75290742c223a24
artemkha <khaapt@gmail.com>
2018-04-27 22:41:25 +0300

    Tag > link > split

--- src/scenes/Topic/tags/index.jsx
+++ src/scenes/Topic/tags/index.jsx
@@ -6,11 +6,16 @@ import { badgeStyle } from '../helpers/badge';
 import configs from 'configs';
 import './tags.css';
 
-
 const Tag = ({ topic }) => (
-  <Link to={`${configs.linkBase()}/topic/${topic.id}`} key={topic.title} className="tags__item">
-    <Badge className="tags__badge" style={badgeStyle(topic.type)}>{' '}</Badge>
-    {' ' + topic.title}
+  <Link
+    to={`${configs.linkBase()}/split/topic/${topic.id}`}
+    key={topic.title}
+    className="tags__item"
+  >
+    <Badge className="tags__badge" style={badgeStyle(topic.type)}>
+      {' '}
+    </Badge>
+    {` ${topic.title}`}
   </Link>
 );
 
@@ -18,20 +23,21 @@ Tag.propTypes = {
   topic: PropTypes.shape({
     id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
-    type: PropTypes.number.isRequired,
-  }).isRequired,
+    type: PropTypes.number.isRequired
+  }).isRequired
 };
 
-const Tags = ({ title, items }) => items[0]
-  ? <div className="tags__tag-box">
-    <span className="tags__title">{title}</span>
-    {items.map(topic => <Tag topic={topic} key={topic.id} />)}
-  </div>
-  : null;
+const Tags = ({ title, items }) =>
+  items[0] ? (
+    <div className="tags__tag-box">
+      <span className="tags__title">{title}</span>
+      {items.map(topic => <Tag topic={topic} key={topic.id} />)}
+    </div>
+  ) : null;
 
 Tags.propTypes = {
   title: PropTypes.string.isRequired,
-  items: PropTypes.array.isRequired,
+  items: PropTypes.array.isRequired
 };
 
 export default Tags;
