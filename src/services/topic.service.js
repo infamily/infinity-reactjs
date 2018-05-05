c58fb9326d9c3a5bc3de3147f1a133a3abc20fbe
Mindey <mindey@qq.com>
2018-05-05 08:04:07 +0000

    don't pass 'parents__isnull' param, if it is 0

--- src/services/topic.service.js
+++ src/services/topic.service.js
@@ -3,33 +3,40 @@ import serverService from './server.service';
 
 const getParams = (flag, view) => {
   const f = flag || '';
-  const params = `lang=${langService.current}&type=${f}&type__not=0&parents__isnull=${view}`;
+  var params = `lang=${langService.current}&type=${f}&type__not=0`;
+  if (view) {
+    params += `&parents__isnull=${view}`;
+  }
   return params;
-}
+};
 
-const logger = (err) => {
+const logger = err => {
   console.log(err);
-}
+};
 
 class TopicService {
   constructor() {
     this.topics = [];
-    this.fromPage = 0; 
+    this.fromPage = 0;
   }
 
   async getTopics(flag, view) {
-    const { data, error } = await serverService.get(`/topics/?${getParams(flag, view)}`)
+    const { data, error } = await serverService.get(
+      `/topics/?${getParams(flag, view)}`
+    );
     if (data) {
       this.topics = data.results;
-      this.fromPage = 1;        
-      return data.results; 
+      this.fromPage = 1;
+      return data.results;
     } else {
       logger(error);
     }
   }
 
   async getPage(page, flag, view) {
-    const { data, error } = await serverService.get(`/topics/?page=${page}&${getParams(flag, view)}`)
+    const { data, error } = await serverService.get(
+      `/topics/?page=${page}&${getParams(flag, view)}`
+    );
     if (data) {
       this.fromPage = page;
       this.topics = data.results;
@@ -40,7 +47,9 @@ class TopicService {
   }
 
   async search(query, flag, view) {
-    const { data, error } = await serverService.get(`/topics/?search=${query}&${getParams(flag, view)}`)
+    const { data, error } = await serverService.get(
+      `/topics/?search=${query}&${getParams(flag, view)}`
+    );
     if (data) {
       this.topics = data.results;
       return data.results;
@@ -52,7 +61,7 @@ class TopicService {
   resetState = () => {
     topicService.fromPage = null;
     topicService.topics = [];
-  }
+  };
 }
 
 const topicService = new TopicService();
