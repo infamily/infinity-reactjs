02ca62702b91b9675375af5c51d956f2c235e576
artemkha <khaapt@gmail.com>
2018-06-10 17:33:56 +0300

    TopicView > check#2 sourceEditorHint

--- src/scenes/TopicView/messages.js
+++ src/scenes/TopicView/messages.js
@@ -97,7 +97,7 @@ export default defineMessages({
   sourceEditorHint: {
     id: 'infinity.scenes.TopicView.sourceEditorHint',
     defaultMessage:
-      'You can create contents in any language. Wrap your title with .:lang:title and your text with .:lang:body to add next language.'
+      'You can create contents in any language. Wrap your title with .:lang:title and your text with .:lang:text to add next language.'
   },
   visualEditorTab: {
     id: 'infinity.scenes.TopicView.visualEditorTab',
