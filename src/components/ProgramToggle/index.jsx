fe6968379de1bb417adec22bc510574d836a26f4
Mindey <mindey@qq.com>
2018-08-23 14:57:08 +0000

    convert ProgramsToggle to side navigation swticher between Goals...Tasks

--- src/components/ProgramToggle/index.jsx
+++ src/components/ProgramToggle/index.jsx
@@ -5,22 +5,43 @@ import { Link } from 'react-router-dom';
 import configs from 'configs';
 import messages from './messages';
 import './ProgramToggle.css';
+import pathToRegexp from 'path-to-regexp';
 
 const ProgramToggle = () => {
   const toTop = () => window.scrollTo(0, 0);
+  const toAll = () => window.scrollTo(0, 0);
+  const toGoals = () => '';
+  const toIdeas = () => '';
+  const toPlans = () => '';
+  const toSteps = () => '';
+  const toTasks = () => '';
+  const toBugs = () =>
+    window.location.replace('https://inf.li/#/wefindx.com:en/@/topic/274/');
 
   return (
     <Link to={configs.linkBase()}>
-      <div className="program_toggle" onClick={toTop}>
-        <TooltipOverlay
-          text={<FormattedMessage {...messages.importingToolip} />}
-          placement="bottom"
-        >
-          <span className="program_toggle__label">
-            <FormattedMessage {...messages.programs} />
-          </span>
-          <span className="program_toggle__sign">&#x03BB;</span>
-        </TooltipOverlay>
+      <div className="program_toggle">
+        <div className="all_toggle__label" onClick={toTop}>
+          <FormattedMessage {...messages.all} />
+        </div>
+        <div className="goal_toggle__label" onClick={toGoals}>
+          <FormattedMessage {...messages.goal} />
+        </div>
+        <div className="idea_toggle__label" onClick={toIdeas}>
+          <FormattedMessage {...messages.idea} />
+        </div>
+        <div className="plan_toggle__label" onClick={toPlans}>
+          <FormattedMessage {...messages.plan} />
+        </div>
+        <div className="step_toggle__label" onClick={toSteps}>
+          <FormattedMessage {...messages.step} />
+        </div>
+        <div className="task_toggle__label" onClick={toTasks}>
+          <FormattedMessage {...messages.task} />
+        </div>
+        <div className="bug_report_toggle__label" onClick={toBugs}>
+          <FormattedMessage {...messages.bug} />
+        </div>
       </div>
     </Link>
   );
--- src/components/ProgramToggle/messages.js
+++ src/components/ProgramToggle/messages.js
@@ -8,5 +8,33 @@ export default defineMessages({
   importingToolip: {
     id: 'infinity.components.ProgramToggle.importingToolip',
     defaultMessage: 'Importing programs will be released coming soon...'
+  },
+  all: {
+    id: 'infinity.components.ProgramToggle.all',
+    defaultMessage: 'All'
+  },
+  goal: {
+    id: 'infinity.components.ProgramToggle.goal',
+    defaultMessage: 'Goals'
+  },
+  idea: {
+    id: 'infinity.components.ProgramToggle.idea',
+    defaultMessage: 'Ideas'
+  },
+  plan: {
+    id: 'infinity.components.ProgramToggle.plan',
+    defaultMessage: 'Plans'
+  },
+  step: {
+    id: 'infinity.components.ProgramToggle.step',
+    defaultMessage: 'Steps'
+  },
+  task: {
+    id: 'infinity.components.ProgramToggle.task',
+    defaultMessage: 'Tasks'
+  },
+  bug: {
+    id: 'infinity.components.ProgramToggle.bug',
+    defaultMessage: 'Bugs'
   }
 });
